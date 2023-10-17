c33257ae2183f77e931ba3b4ce19a0fedea7c8ab
Dominique Grys <grysdominique@gmail.com>
2023-10-17 16:40:40 +0300

    about us text

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    let navLinks = document.querySelector('.nav-links');
-    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
-});
